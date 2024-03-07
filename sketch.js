805c031a84c253a2bcac4850e9612af76311b629
yHwDev <161657875+yHwDev@users.noreply.github.com>
2024-03-07 19:38:45 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+function preload(){
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+}
+
+function setup(){
+
+createCanvas(400,400);
+
+  path=createSprite(200,200);
+  path.addImage(pathImg);//
+  path.velocityY = 4;
+  path.scale=1.2;//
+
+  boy = createSprite(180,340,30,30);
+  boy.scale=0.08;
+  boy.addAnimation("JakeRunning",boyImg);
+
+leftBoundary=createSprite(-30,0,100,800);
+leftBoundary.visible = false
+
+rightBoundary=createSprite(440,0,100,800);
+rightBoundary.visible = false
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  
+  boy.x = World.mouseX
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary)
+  
+  //código para redefinir o fundo
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+
+
+  drawSprites();
+} 
